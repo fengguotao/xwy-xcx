673c664189c29c9fa531d0c64a233f4b8155b4c7
Fenguotao <fengguotao@levect.com>
2017-11-15 09:35:03 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,71 @@
+//index.js
+//获取应用实例
+var config = require("../../utils/config.js")
+Page({
+  data: {
+    imgUrls: [
+      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,//显示面板指示点
+    autoplay: true,//自动轮播
+    interval: 5000,
+    duration: 1000,
+    list : [
+    'aaaa','bbbbb','ccccc'
+    ]
+  },
+  onLoad: function () {
+    var that = this
+    console.log(config.getListJson)
+    wx.request({
+      url: config.getListJson,
+      data: {
+        columnId : '100000002',
+        language : 'zh',
+        size : 50,
+        page : 0
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        let info = res.data.info;
+        let list = res.data.list;
+        console.log(res.data)
+        //this.data.imgUrls = info.picLinks
+        that.setData({
+          info : info,
+          imgUrls: [info.picLinks],
+          list: list
+        })
+      }
+    })
+  },
+  changeIndicatorDots: function() {
+    wx.navigateTo({
+      url: '../webview/webview'
+    })
+    // this.setData({
+    //   indicatorDots: !this.data.indicatorDots
+    // })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '自定义转发标题',
+      path: '/page/user?id=123',
+      success: function(res) {
+        // 转发成功
+      },
+      fail: function(res) {
+        // 转发失败
+      }
+    }
+  }
+})
---
+++ utils/config.js
@@ -0,0 +1,12 @@
+/**
+ * 小程序配置文件
+ */
+// 此处主机域名是腾讯云解决方案分配的域名
+// 小程序后台服务解决方案：https://www.qcloud.com/solution/la
+//var host = "http://coffeeapi-web.tunnel.qydev.com/CoffeeAPI-Web" //测试
+var host = "http://api.wx.china.cn/" //测试
+var config = {
+  getListJson: `${host}/activity/list/json?`,
+
+};
+module.exports = config
